e38f1594669a235164fcc572c10a6e6eddbaaf7a
Devanand-web-profile <103214086+Devanand-web-profile@users.noreply.github.com>
2022-04-13 13:10:39 +0530

    Add files via upload

---
+++ for loop1.py
@@ -0,0 +1,62 @@
+
+'''
+for x in range(10):
+    print(x,end=" ")
+print()   
+for x in range(1,11):
+    print(x,end=" ")
+print()
+for x in range(1,20,3):
+    print(x,end=" ")
+print()    
+for x in range(1,11):
+    print(x*x*x,end=" ")
+print()
+for x in range(1,11):
+    print(x*9,end =" ")
+    
+'''
+'''
+for x in range(1,11,2):
+    print(x*x)
+print()
+
+for x in range(0,11,2):
+    print(x*x,end=" ")
+'''
+'''
+for x in range(1,11):
+    if x==5:
+        print("x in break ",x)
+        break
+    else:
+        print("x is ",x)
+'''
+
+for x in range(1,11):
+    if x%2==0:
+        print("even ",x)
+print()    
+for x in range(1,11):
+    if x%2==1:
+        print(x)
+print()       
+for x in range(1,11):
+    if x%2!=0:
+        print(x)
+        
+print()
+
+for x in range(1,51):
+    if x%3==0 and x%7==0:
+        print(x)
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
